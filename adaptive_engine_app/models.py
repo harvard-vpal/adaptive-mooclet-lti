309e6cf015ffad3f9755ed7950e35ca6ac7ec907
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-04-27 08:34:24 -0400

    Added initial model implementation.

--- adaptive_engine_app/models.py
+++ adaptive_engine_app/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Component (models.Model):
+	component_name = models.CharField(max_length=64)
+
+class Version (models.Model):
+	component = models.ForeignKey(Component, on_delete=models.CASCADE)
+	text = models.CharField(max_length=8192)
+
+class Student (models.Model):
+	student_id = models.CharField(max_length=64, primary_key=True)  # From LTI
+
+class Result (models.Model):
+	student = models.ForeignKey(Student, on_delete=models.CASCADE)
+	version = models.ForeignKey(Version, on_delete=models.CASCADE)
+	result = models.FloatField()
--- django_adaptive_engine/settings.py
+++ django_adaptive_engine/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adaptive_engine_app.apps.AdaptiveEngineAppConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
