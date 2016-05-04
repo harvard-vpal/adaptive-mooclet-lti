6e13475a8f77d30bb9827986ee6033f7b7806dfc
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-05-04 14:54:12 -0400

    Removed check for redundant (student,version_id) rows.

--- adaptive_engine_app/views.py
+++ adaptive_engine_app/views.py
@@ -37,10 +37,6 @@ def submit_result_of_version (request):
 	student = request.GET['student']
 	value = float(request.GET['value'])
 
-	# Make sure there's not already an entry in the database for this (student,version) combination
-	if Result.objects.filter(student=student, version_id=version_id).count() == 0:
-		result = Result(student=student, version_id=version_id, value=value)
-		result.save()
-		return HttpResponse('Ok')
-	else:
-		return HttpResponse('Error: attempt to submit duplicate entry!')
+	result = Result(student=student, version_id=version_id, value=value)
+	result.save()
+	return HttpResponse('Ok')
