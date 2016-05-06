7a987b34fd3dca7b587530f1ec84b95ac5246aab
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-05-06 09:07:13 -0400

    Fixed typo.

--- adaptive_engine_app/admin.py
+++ adaptive_engine_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Component, Version
+from .models import Component, Version, Result
 
 admin.site.register(Component)
 admin.site.register(Version)
