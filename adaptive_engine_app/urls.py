2dd4f90afcbaf4d7ad3cac418930fd36f88e8828
Jacob Whitehill <jacob_whitehill@harvard.edu>
2016-05-10 15:36:41 -0400

    Fixed typos.

--- adaptive_engine_app/urls.py
+++ adaptive_engine_app/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^get_explanation$', 'adaptive_engine_app.views.get_explanation', name='get_explanation'), # http://localhost:8000/adaptive_engine_app/get_explanation
-    url(r'^submit_result_of_explanation$', 'adaptive_engine_app.views.submit_result_of_explanation', name='submit_result_of_explanation')
+    url(r'^submit_result_of_explanation$', 'adaptive_engine_app.views.submit_result_of_explanation', name='submit_result_of_explanation'),
+    url(r'^add_question$', 'adaptive_engine_app.views.add_question', name='add_question'),
+    url(r'^add_explanation$', 'adaptive_engine_app.views.add_explanation', name='add_explanation')
 )
--- adaptive_engine_app/views.py
+++ adaptive_engine_app/views.py
@@ -5,21 +5,34 @@ from .models import Question, Explanation, Result
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 
-def add_quiz_to_course (request):
-	pass
-
-def add_question_to_quiz (request):
-	pass
-
-def add_answer_to_question (request):
-	pass
+def add_question (request):
+	if 'text' not in request.GET:
+		return HttpResponse('text not found in GET parameters')
+	if 'answer1' not in request.GET:
+		return HttpResponse('answer1 not found in GET parameters')
+	if 'answer2' not in request.GET:
+		return HttpResponse('answer2 not found in GET parameters')
+	if 'answer3' not in request.GET:
+		return HttpResponse('answer3 not found in GET parameters')
+	if 'answer4' not in request.GET:
+		return HttpResponse('answer4 not found in GET parameters')
+	question = Question(text=request.GET['text'], answer1=request.GET['answer1'], \
+	                                              answer2=request.GET['answer2'], \
+	                                              answer3=request.GET['answer3'], \
+	                                              answer4=request.GET['answer4'])
+	question.save()
+	return JsonResponse({ "received": "yes" })
 
 def add_explanation (request):
 	if 'question_id' not in request.GET:
 		return HttpResponse('question_id not found in GET parameters')
-	if 'student_id' not in request.GET:
-		return HttpResponse('student_id not found in GET parameters')
-	pass
+	if 'answer_id' not in request.GET:
+		return HttpResponse('answer_id not found in GET parameters')
+	if 'text' not in request.GET:
+		return HttpResponse('text not found in GET parameters')
+	explanation = Explanation(question_id=request.GET['question_id'], answer_id=int(request.GET['answer_id']), text=request.GET['text'])
+	explanation.save()
+	return JsonResponse({ "received": "yes" })
 
 def get_explanation (request):
 	if 'question_id' not in request.GET:
