00cbd22e0ba55f37590e23bb7412a4c878aa4989
Andrew Ang <ang.kunanit@gmail.com>
2016-08-18 11:07:23 -0400

    More removal of patterns usage

--- qualtrics/urls.py
+++ qualtrics/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^create_qualtrics_quiz_from_url$', views.create_qualtrics_quiz_from_url, name='create_qualtrics_quiz_from_url'),
 	url(r'^qsf_for_question/(?P<question_id>[0-9]+)$', views.qsf_for_question, name='qsf_for_question'),
     url(r'^end_quiz$', views.end_quiz, name='end_quiz'),
 
-)
+]
