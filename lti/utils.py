13d553605509932d29149619023e423125a37e39
Andrew Ang <ang.kunanit@gmail.com>
2016-09-01 08:52:03 -0400

    rename quiz_lti_params to lti_params

--- lti/utils.py
+++ lti/utils.py
@@ -43,8 +43,8 @@ def grade_passback(grade=None, request=None, user=None, quiz=None):
         
     elif user and quiz:
         lti_parameters = user.ltiparameters_set.filter(quiz=quiz)
-        if quiz_lti_parameters.exists():
-            LTI_LAUNCH = quiz_lti_parameters.values()[0]
+        if lti_parameters.exists():
+            LTI_LAUNCH = lti_parameters.values()[0]
     else:
         pass
 
